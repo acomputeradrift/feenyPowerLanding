bd1078e6cd8ef37e857f66a0d2144e9951035062
acomputeradrift <feeny.jamie@gmail.com>
2025-03-16 03:25:31 +0000

    Added a new page for diagnostics upload and changed the default page url to consultation

--- fpc_server.js
+++ fpc_server.js
@@ -7,8 +7,20 @@ const app = express();
 // Serve static files from the feenyPowerLanding directory
 app.use(express.static('/root/feenyPowerLanding'));
 
+// Redirect root URL to /consultation/
 app.get('/', (req, res) => {
+    res.redirect('/consultation');
+});
+
+
+// Change the landing page URL to /consultation/
+app.get('/consultation', (req, res) => {
     res.sendFile(path.join('/root/feenyPowerLanding', 'landing.html'));
+});
+
+// Serve the RTI Diagnostics Page
+app.get('/rti_diagnostics', (req, res) => {
+    res.sendFile(path.join('/root/feenyPowerLanding', 'rti_diagnostics.html'));
 });
 
 // MongoDB Connection Example
@@ -22,3 +34,32 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
+
+
+
+
+// const path = require('path');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const app = express();
+
+// // Middleware to parse JSON
+// // Serve static files from the feenyPowerLanding directory
+// app.use(express.static('/root/feenyPowerLanding'));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join('/root/feenyPowerLanding', 'landing.html'));
+// });
+
+// // MongoDB Connection Example
+// const dbURI = process.env.MONGO_URI || 'mongodb://localhost:27017/testdb';
+// mongoose.connect(dbURI)
+//     .then(() => console.log('MongoDB connected'))
+//     .catch(err => console.error('MongoDB connection error:', err));
+
+// // Start the server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server running at http://localhost:${PORT}`);
+// });
